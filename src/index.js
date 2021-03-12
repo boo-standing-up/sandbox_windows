9683ac275d8f8df41cb64d0864cd9284e7a74bbd
boo-standing-up <70996914+boo-standing-up@users.noreply.github.com>
2021-03-12 16:18:54 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -20,3 +20,55 @@
 //  boo.push("たぬき");
 //  console.log(boo[0]);
 //  console.log(boo[2]);
+
+/**ログにテンプレート文字列を入れてもだめ */
+// const name = "boo";
+// const age = 14;
+// console.log('私の名前は${name}です。年齢は${age}です。');
+
+const name = "boo";
+const age = 14;
+const messege = `私の名前は${name}です。年齢は${age}です。`;
+console.log(messege);
+
+/**
+ * アロー関数
+ */
+
+// //従来の関数
+// function func1(x) {
+//   return x + 1;
+// }
+// console.log(func1("4"));
+
+// const func1 = function (x) {
+//   return x + 1;
+// };
+// console.log(func1("4"));
+
+// const func1 = (x) => {
+//   return x + 1;
+// };
+// console.log(func1("4"));
+
+//一行で処理が終わる場合、return｛｝省略して一行でかける
+
+// const func1 = (x) => x + 1;
+// console.log(func1("4"));
+
+// const boosan = (num1, num2) => num1 + num2 + num1 * 3;
+// console.log(boosan(4, 5));
+
+// 分割代入
+
+const booBody = {
+  name: "boo",
+  age: 14
+};
+
+const messege1 = `私の名前は${booBody.name}です。`;
+console.log(messege1);
+
+// const { name, age } = booBody;
+// const messege5 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(messege5);
